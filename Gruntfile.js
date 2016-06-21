8cb89f77f96efae4e38ff469852661307854bdc2
James Barnsley <james@barnsley.nz>
2016-06-21 15:10:12 +1200

    Updating docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 banner: '/**\n * <%= pkg.name %>\n * Built <%= grunt.template.today("yyyy-mm-dd") %>\n **/\n\n',
-				ASCIIOnly: 'true'
+				ASCIIOnly: 'true',
+				drop_console: true
             },
             build: {
                 src: 'site/production/site.js',
@@ -29,18 +30,6 @@ module.exports = function(grunt) {
 				}
 			}
         },
-        cssmin: {
-            options: {
-                processImport: false,
-                shorthandCompacting: false,
-                roundingPrecision: -1
-            },
-            build: {
-                files: {
-                    'site/production/site.min.css': ['site/production/site.css']
-                }
-            }
-        },
 		watch: {
 			scripts: {
 				files: ['site/js/*.js'],
@@ -48,7 +37,7 @@ module.exports = function(grunt) {
 			},
 			css: {
 				files: 'site/scss/**/*.scss',
-				tasks: ['cssmin','sass']
+				tasks: ['sass']
 			}
 		}
     });
@@ -56,11 +45,10 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin', 'watch']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
 
 };
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 ## Skeletor
 
 This is the base platform for new installs on the SilverStripe framework. It indicates the preferred approach for all of our projects.
+
+
+# Requirements
+
+- GruntJS
+- Node Package Manager (npm)
+- SilverStripe 3.2
+
+
+# Setup
+
+1. Clone this repository to your local environment
+2. Create your `_ss_environment.php` file (use the example if necessary). Remember to keep this outside of the `public_html` folder!
+3. Compile your PHP dependencies with Composer `composer install`
+4. Compile your Javascript dependencies with NPM `npm install`
+5. Build your SilverStripe environment `/dev/build?flush=all`
+6. Watch for sass and js file changes `grunt` or `grunt &` to run in the background
