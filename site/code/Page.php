a18fdd1b45b74dc6c30cd7d012068b8b7d28cb6a
Jeremy <jeremy@plasticstudio.co.nz>
2017-02-22 14:11:47 +1300

    Email send functiona and template

--- site/code/Page.php
+++ site/code/Page.php
@@ -46,4 +46,120 @@ class Page_Controller extends ContentController {
 			Requirements::css('site/production/index.css');
 		}
 	}
+
+
+	/**
+	 * Send email to admin email address defined in SiteConfig
+	 * @param array $TemplateData array of data to pass to template. Should be in this format:
+	 *                            array(
+	 *                            	'Subject' => 'The subject line of the email',
+	 *                            	'Title' => 'The email title. Can contain html.',
+	 *                            	'Description' => 'The email description. Can contain html.',
+	 *                            	'ReplyToEmail' => $data['Email'], // Optional, email address that was captured in the form
+	 *                            	'Fields' => ArrayList::create(// ArrayList of the data you want to output in the email, in key=>val pairs, eg:
+	 *								    array(
+	 *										ArrayData::create(array('Key' => 'Name', 'Value' => $data['Name'])),
+	 *										ArrayData::create(array('Key' => 'Email', 'Value' => $data['Email'])),
+	 *										ArrayData::create(array('Key' => 'Phone', 'Value' => $data['Phone'])),
+	 *										ArrayData::create(array('Key' => 'Message', 'Value' => $data['Message']))
+	 *									)
+	 *								),
+	 *                            	'SourceLink' => 'Link to page email was sent from (defaults to $this->Link)'
+	 *                            )
+	 *                            
+	 */
+	function EmailAdmin($TemplateData=array()){
+
+		// can only send if AdminEmails have been set
+		if($this->SiteConfig()->AdminToEmails){
+
+			// set email variables
+			if($this->SiteConfig()->AdminFromEmail){
+				$from = $this->SiteConfig()->AdminFromEmail;
+			}else{
+				$from = 'noreply@plasticstudio.co';
+			}
+			$to = $this->SiteConfig()->AdminToEmails;
+			if(isset($TemplateData['Subject'])){
+				$subject = $TemplateData['Subject'];
+			}else{
+				$subject = 'Email has been submitted through '.$this->SiteConfig()->Title;
+			}
+			$body = '';
+			$email = new Email($from, $to, $subject, $body);
+			$email->setBcc('client@plasticstudio.co.nz');
+			if(isset($TemplateData['ReplyToEmail'])) $email->replyTo($TemplateData['ReplyToEmail']);
+			
+			// set template
+			// NOTE: Email template references a logo at /site/images/email-template-logo.png
+			// so make sure that file has been added
+	    	$email->setTemplate('Email');
+			
+	    	// populate template
+	    	$email->populateTemplate(array('TemplateData' => $TemplateData));
+			
+	    	// send mail
+	    	return $email->send();
+
+		}
+		
+	}
+
+	/**
+	 * Send email to form submitter
+	 * @param array $TemplateData array of data to pass to template. Should be in this format:
+	 *                            array(
+	 *                            	'Subject' => 'The subject line of the email',
+	 *                            	'Title' => 'The email title. Can contain html.',
+	 *                            	'Description' => 'The email description. Can contain html.',
+	 *                            	'ToEmail' => $data['Email'], // email address that was captured in the form
+	 *                            	'Fields' => ArrayList::create(// ArrayList of the data you want to output in the email, in key=>val pairs, eg:
+	 *								    array(
+	 *										ArrayData::create(array('Key' => 'Name', 'Value' => $data['Name'])),
+	 *										ArrayData::create(array('Key' => 'Email', 'Value' => $data['Email'])),
+	 *										ArrayData::create(array('Key' => 'Phone', 'Value' => $data['Phone'])),
+	 *										ArrayData::create(array('Key' => 'Message', 'Value' => $data['Message']))
+	 *									)
+	 *								),
+	 *                            	'SourceLink' => 'Link to page email was sent from (defaults to $this->Link)'
+	 *                            )
+	 *                            
+	 */
+	function SubmitterAdmin($TemplateData=array()){
+
+		// can only send if AdminEmails have been set
+		if(isset($TemplateData['ToEmail'])){
+
+			// set email variables
+			if($this->SiteConfig()->AdminFromEmail){
+				$from = $this->SiteConfig()->AdminFromEmail;
+			}else{
+				$from = 'noreply@plasticstudio.co';
+			}
+			$to = $TemplateData['ToEmail'];
+			if(isset($TemplateData['Subject'])){
+				$subject = $TemplateData['Subject'];
+			}else{
+				$subject = 'Your submission has been received through '.$this->SiteConfig()->Title;
+			}
+			$body = '';
+			$email = new Email($from, $to, $subject, $body);
+			$email->setBcc('client@plasticstudio.co.nz');
+			
+			// set template
+			// NOTE: Email template references a logo at /site/images/email-template-logo.png
+			// so make sure that file has been added
+	    	$email->setTemplate('Email');
+			
+	    	// populate template
+	    	$email->populateTemplate(array('TemplateData' => $TemplateData));
+			
+	    	// send mail
+	    	return $email->send();
+
+		}
+		
+	}
+
+
 }
--- site/templates/Layout/Page.ss
+++ site/templates/Layout/Page.ss
@@ -6,6 +6,7 @@
 
 			<h1>$Title</h1>
 			$Content
+			$ContactForm
 			$Form
 
 		</main>
