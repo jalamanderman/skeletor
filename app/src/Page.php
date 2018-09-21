1fbc83fc0efa1a0c599627bac2fe368f15a45cf6
James Barnsley <james@barnsley.nz>
2018-09-21 14:30:50 +1200

    Tables

--- app/src/Page.php
+++ app/src/Page.php
@@ -5,27 +5,20 @@ use SilverStripe\CMS\Model\SiteTree;
 
 class Page extends SiteTree {
 
-	private static $db = array(
+	private static $db = [
 		'MetaTitle' => 'Text'
-	);
+	];
 
-	private static $has_one = array();
-	
-	public function getCMSFields(){	
+	public function getCMSFields(){
 		$fields = parent::getCMSFields();
 
-		$fields->addFieldToTab('Root.Main.Metadata', TextField::create('MetaTitle','Meta Title')->setRightTitle('Customised title for use in search engines. Defaults to the page title.'), 'MetaDescription');
+		$fields->addFieldToTab(
+			'Root.Main.Metadata',
+			TextField::create('MetaTitle','Meta Title')
+				->setRightTitle('Customised title for use in search engines. Defaults to the page title.'),
+			'MetaDescription'
+		);
 
 		return $fields;
 	}
-	
-	/**
-	 * Get this model's controller
-	 * @return obj
-	 */
-	public function MyController(){
-		$class = $this->ClassName . "_Controller";
-		$controller = new $class($this);
-		return $controller;
-	}	
 }
--- app/src/PageController.php
+++ app/src/PageController.php
@@ -10,19 +10,13 @@ class PageController extends ContentController {
 
 	public function init() {
 		parent::init();
-
-		// global compiled javascript
+		
 		if (Director::isLive()){
 			Requirements::javascript('app/production/index.min.js');
-		} else {
-			Requirements::javascript('app/production/index.js');
-		}
-
-		// global css (compiled scss)
-		if (Director::isLive()){
 			Requirements::css('app/production/index.min.css');
 		} else {
 			Requirements::css('app/production/index.css');
+			Requirements::javascript('app/production/index.js');
 		}
 	}
 }
